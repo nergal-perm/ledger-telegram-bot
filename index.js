277ad3edecbc71ee55acae9d6f5530e68abbd7fa
nergal-perm <nergal@psu.ru>
2018-05-07 11:37:53 +0500

    fetching config properties done right

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 require("isomorphic-fetch");
 
-// Dropbox official javascript client setup
-var Dropbox = require("dropbox").Dropbox;
-var dbx = new Dropbox({accessToken: conf.accessToken});
-
 // external file configuration setup
 var conf = require("nconf");
 conf.argv()
     .env()
     .file('main', { file: 'config/main.json', search: true });
 
+// Dropbox official javascript client setup
+var Dropbox = require("dropbox").Dropbox;
+var dbx = new Dropbox({accessToken: conf.get('accessToken')});
 
-dbx.filesDownload({ path: conf.filePath })
+dbx.filesDownload({ path: conf.get('filePath') })
     .then(function(response) {
         var buff = new Buffer(response.fileBinary);
         var stringToWrite = "\nJust some sample text here...";
@@ -25,7 +24,7 @@ dbx.filesDownload({ path: conf.filePath })
 function uploadFile(fileContent) {
     dbx.filesUpload({
         contents: fileContent,
-        path: conf.filePath,
+        path: conf.get('filePath'),
         mode: "overwrite"
     }).then(function(response) {
         console.log(JSON.stringify(response));
